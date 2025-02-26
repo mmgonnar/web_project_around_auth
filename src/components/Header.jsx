73dc82aeb5bea97a83d777cb0839e771ff449c4a
mmgonnar <gn.mariela@gmail.com>
2025-02-25 18:52:46 -0600

    Finnished login css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,10 @@ const Header = () => {
         className="header__logo"
       />
       <div className="header__text-container">
-        <p className="header__text">Logout</p>
-        <img
-          src="/public/icons/sign_out.svg"
-          className="header__icon"
-          alt="Sign out icon"
-        />
+        <a className="header__text" href="/">
+          Dinamic Text
+        </a>
+        {/* <button className="button button_edit-avatar"></button> */}
       </div>
     </header>
   );
